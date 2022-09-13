d2689a6629de5feda06ee39e848a88a9e550b7fe
vagnerturella <vagnertrll@gmail.com>
2022-09-13 11:54:25 -0300

    reparo no botao de fechar menu

--- js/navbar.js
+++ js/navbar.js
@@ -11,12 +11,15 @@ function toggleNavBar(){
   if(!open){
    navBar.style.left = "0px";
     hamburger.classList.add("active");
+    document.querySelector('.icon-hamb').src="css/img/svg/icons8-fechar-janela-48.png";
     body.style.overflow = "hidden";
    open = true;
   }else{
     navBar.style.left = "-269px";
     hamburger.classList.remove("active");
+    document.querySelector('.icon-hamb').src="css/img/svg/icons8-cardápio-24.png";
     body.style.overflow = "visible";
     open = false;
   }   
 }
+
